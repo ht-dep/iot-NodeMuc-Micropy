e5d28914e6e39efd248c816bcbac118017de1f51
ht <1009510247@qq.com>
2017-05-06 16:31:21 +0800

    修改tcp穿透

--- IOTPRO/启动.bat
+++ IOTPRO/启动.bat
@@ -6,13 +6,13 @@ set path=%SystemRoot%\System32;
 
 REM @%SystemRoot%\System32\cmd /k
 
-start "消息队列---redis服务" C:\Redis\redis-server.exe &
-start "内网穿透---NAT服务" natNet.py &
-start "中间件---MIDDLE服务" server_middle.py &
+start  "消息队列---redis服务" /min C:\Redis\redis-server.exe  &
+start "内网穿透---NAT服务" /min natNet.py BIN  &
+start "中间件---MIDDLE服务" /min server_middle.py   &
  
-start "WEB---FLASK服务" web_server.py &
+start "WEB---FLASK服务" /min web_server.py   &
 
-start "后台---WEBSOCKET服务" websocket_end.py &
+start "后台---WEBSOCKET服务" /min  websocket_end.py   &
 
 
 
